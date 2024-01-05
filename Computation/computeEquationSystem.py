3e68b8eab2dd858475a4d5d722f14ab11031a17d
Anson Zeng <ansonzeng@gmail.com>
2024-01-05 15:08:33 +0800

    Add the comment

--- Computation/computeEquationSystem.py
+++ Computation/computeEquationSystem.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-A = np.array([[2,-1],[1,1]]) 
+#Define the matrix
+A = np.array([[2,-1],[1,1]])
+#Define the result vector 
 b = np.array([1,5]) 
+#Solve the linear equation system
 print(np.linalg.solve(A,b))
